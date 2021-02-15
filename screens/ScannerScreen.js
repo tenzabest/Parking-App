ce62c657c4e05b34de5845c66b68401599435a3b
Abdoullah <tenzabest2001@gmail.com>
2021-02-15 12:32:00 -0500

    set timer between each scan instead of the button

--- screens/ScannerScreen.js
+++ screens/ScannerScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet} from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import * as Permissions from 'expo-permissions';
 import { updateStudent } from "../firebaseService"
@@ -32,36 +32,32 @@ class ScannerScreen extends Component {
     return (
 
       <View style={styles.container}>
-
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
           style={StyleSheet.absoluteFillObject}
         />
-
-        {scanned && (
-          <Button
-            title={'Tap to Scan Again'}
-            onPress={() => this.setState({ scanned: false })}
-          />
-        )}
       </View>
     );
   }
 
   handleBarCodeScanned = ({ type, data }) => {
-    this.setState({ scanned: true });
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-    let temp = data
+     this.setState({ scanned: true });
+        let temp = data
     if (data.length === 1) {
       temp = "00" + data
     } else if (data.length === 2) {
       temp = "0" + data
 
     }
+  
     updateStudent(temp)
       .catch((error) => {
         console.log(error);
       });
+  
+  setTimeout(()=>{
+    this.setState({ scanned: false });
+  }, 1500);
   };
 }
 const styles = StyleSheet.create({
